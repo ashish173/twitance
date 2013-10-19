2ed988906b3e9af40b5f12dcf47f7475248bf5df
ashish173 <aitashish173@gmail.com>
2013-10-20 04:59:56 +0530

    cleaned controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,13 +1,7 @@
 class WelcomeController < ApplicationController
+  # just for the sake of testing
   def index
-     # @tweet =  USERHANDLE.search("#ruby", :lang => "en", :count => 1).results.first.text
-     # @tweet =  USERHANDLE.retweets_of_me
-    # @tweet = USERHANDLE.mentions_timeline # returns the timeline
-    #@tweet = USERHANDLE.retweeters_of(388205958628790272, options={})
-    # @tweet = USERHANDLE.home_timeline
-     #@tweet = USERHANDLE.retweeters_of(388442551012691968, options={})
-    # @tweet = USERHANDLE.user_timeline("ashishait")
-      @tweet = USERHANDLE.user("rohitkhatana3")
+    @tweet = USERHANDLE.user("rohitkhatana3")
     p "my tweet" # show on console
     p @tweet     
   end
