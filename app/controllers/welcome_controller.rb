87cbae46894d35c2d53caae3baa36d6ec192a5fc
ashish173 <aitashish173@gmail.com>
2013-10-20 04:56:06 +0530

    added more rake tasks

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,14 @@
+class WelcomeController < ApplicationController
+  def index
+     # @tweet =  USERHANDLE.search("#ruby", :lang => "en", :count => 1).results.first.text
+     # @tweet =  USERHANDLE.retweets_of_me
+    # @tweet = USERHANDLE.mentions_timeline # returns the timeline
+    #@tweet = USERHANDLE.retweeters_of(388205958628790272, options={})
+    # @tweet = USERHANDLE.home_timeline
+     #@tweet = USERHANDLE.retweeters_of(388442551012691968, options={})
+    # @tweet = USERHANDLE.user_timeline("ashishait")
+      @tweet = USERHANDLE.user("rohitkhatana3")
+    p "my tweet" # show on console
+    p @tweet     
+  end
+end
---
+++ lib/tasks/tweets.rb
@@ -0,0 +1,26 @@
+
+namespace :run do
+  task :greet do
+    puts "Hello world"
+  end
+
+  task :gettweet => :environment do
+    @tweet = USERHANDLE.user("ashishait") 
+    @tweet = @tweet.to_json()
+    # puts @tweet.class
+    @hash = JSON.parse(@tweet)
+    # parse turns json to hash object
+    # puts @hash['status']['text']
+    # puts @hash.class
+    @hash.each_key().each do |key|
+      puts "--> #{key} -> #{@hash[key]}"
+    end
+  end
+ 
+  
+
+  task :all => [:greet, :gettweet]  
+  # You don't need to supply a block of code to all task
+
+end
+
