fc836da150eef167c6dfc6aee0083159b8396ab1
ashish173 <aitashish173@gmail.com>
2013-12-08 10:44:33 +0530

    download tweet for #Ruby

--- ratelimits/download_tweets.rb
+++ ratelimits/download_tweets.rb
@@ -4,18 +4,19 @@ require 'tire'
 
 
 TweetStream.configure do |config|
-    config.consumer_key       = 'kgIpenTTZcD7wD9dnOOxPQ'
-    config.consumer_secret    = 'hHWxrOaZ3e1P9q44q6t0tNLvHZXucKETg2CGuchsmRc'
-    config.oauth_token        = '838203445-5H2Qq5gRceSmLMXmHgtzDDWPUmGMzbEXRJtLjRNv'
-    config.oauth_token_secret = 'DMvzqeNW6DhqM26lUyx4VhnM3s5NlwmLQ4IV9fuORHg'
+    config.consumer_key       = 'YPC3v1fzpnJHeSfTNH2ZhQ'
+    config.consumer_secret    = 'bo6Or0GpaymfVccdaSliG8OGYHr4aYEtshZu4W2lCY'
+    config.oauth_token        = '283990956-4SEvREjo1CmUWK4Lb9k8Kp2U5FkAMaVki46IxOAS'
+    config.oauth_token_secret = 'Gm57bk6esWS8VAIZNMazTn8o5tqPd3pDZqCBAYR8U'
     config.auth_method        = :oauth
 end
-@index = "nike"
+@index = "ruby"
 #@index = "FCBarcelona"
 #@index = "thingsiliketosee"
 # This will pull a sample of all tweets based on
 # your Twitter account's Streaming API role.
-TweetStream::Client.new.track(@index) do |status|
+
+TweetStream::Client.new.track(@index, "rails", "rubyonrails", "rubygem", "#ruby", "#rubyonrails", "#rubygem") do |status|
   # The status object is a special Hash with
   # method access to its keys.
   puts "Tweet: #{status.text}"
@@ -23,15 +24,17 @@ TweetStream::Client.new.track(@index) do |status|
   puts "Id: #{status.id}" 
   puts "Location: #{status.user.location}"
 
-  file = File.new("new_tweets_16","a+")
+  file = File.new("new_tweets_ruby1","a+")
   # put "file descriptpr: #{file}"
+  @tweet_json = status.to_json()
   if file 
-    file.syswrite("[Tweet: #{status.text}")#,#{status.text},#{status.created_at},#{status.user.screen_name},#{status.location}")
-    file.syswrite(", created_at: #{status.created_at}")
-    file.syswrite(", location: #{status.user.location}")
-    file.syswrite(", user_id: #{status.id}],")
+    file.syswrite("#{@tweet_json},")
+    #,#{status.text},#{status.created_at},#{status.user.screen_name},#{status.location}")
+    #file.syswrite(", created_at: #{status.created_at}")
+    #file.syswrite(", location: #{status.user.location}")
+    #file.syswrite(", user_id: #{status.id}],")
     file.syswrite("\n")
-    # file.close()
+    file.close()
   else
     puts "could not open file"
   end
@@ -42,6 +45,11 @@ TweetStream::Client.new.track(@index) do |status|
   #  import list
   #  refresh
   #end
-=end  
+=end
 end
+=begin
+TweetStream::Client.new.sample do |status|
+  puts "#{status.text}"
+end
+=end
 
