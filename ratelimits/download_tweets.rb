12d787d4f5766cd86d4b9874d45f898a961d9ad4
ashish173 <aitashish173@gmail.com>
2013-12-24 17:46:09 +0530

    added rake file for filter

--- ratelimits/download_tweets.rb
+++ ratelimits/download_tweets.rb
@@ -11,45 +11,21 @@ TweetStream.configure do |config|
     config.auth_method        = :oauth
 end
 @index = "ruby"
-#@index = "FCBarcelona"
-#@index = "thingsiliketosee"
 # This will pull a sample of all tweets based on
 # your Twitter account's Streaming API role.
 
 TweetStream::Client.new.track(@index, "rails", "rubyonrails", "rubygem", "#ruby", "#rubyonrails", "#rubygem") do |status|
-  # The status object is a special Hash with
-  # method access to its keys.
   puts "Tweet: #{status.text}"
-  puts "Date: #{status.created_at}"
-  puts "Id: #{status.id}" 
-  puts "Location: #{status.user.location}"
 
   file = File.new("new_tweets_ruby3","a+")
   # put "file descriptpr: #{file}"
   @tweet_json = status.to_json()
   if file 
     file.syswrite("#{@tweet_json},")
-    #,#{status.text},#{status.created_at},#{status.user.screen_name},#{status.location}")
-    #file.syswrite(", created_at: #{status.created_at}")
-    #file.syswrite(", location: #{status.user.location}")
-    #file.syswrite(", user_id: #{status.id}],")
     file.syswrite("\n")
     file.close()
   else
     puts "could not open file"
   end
-=begin
-  list = [{:type => @index, :tweet => status.text, :created_at => status.created_at, :user_name => status.user.screen_name }]
-  
-  #Tire.index 'twitdata' do
-  #  import list
-  #  refresh
-  #end
-=end
 end
-=begin
-TweetStream::Client.new.sample do |status|
-  puts "#{status.text}"
-end
-=end
 
